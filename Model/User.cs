c313733cd060a5d0647b8eb78effbdf63dc4f5dc
AdnanNazirShalla <94174024+AdnanNazirShalla@users.noreply.github.com>
2022-05-18 20:52:39 -0700

    model added

---
+++ Model/User.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    public class UserRequest
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="Required!")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Required!")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Required!")]
+        public Gender Gender { get; set; }
+
+
+        [Required(ErrorMessage = "Required!")]
+        public UserRole userRole { get; set; }
+
+
+        [Required(ErrorMessage = "Required!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string _password { get; set; }
+
+        [Required(ErrorMessage = "Required!")]
+        [Compare(nameof(_password), ErrorMessage = "Password Does Not Match!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+      
+        public string _confirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Required!")]
+        public double Balance { get; set;}
+
+
+        [Required(ErrorMessage = "Required!")]
+        public int _pin { get; set; }
+    }
+
+
+
+    public class LoginRequest
+    {
+        [Required(ErrorMessage = "Required!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+       
+        public bool RememberMe { get; set; }
+    }
+
+    public class LoginResponse: UserRequest
+    {
+        public bool HasError { get; set; }
+    }
+}
